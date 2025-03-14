03e5eb968955046a07166f360f480d77ff25242c
Jacky0624 <a0987143311@gmail.com>
2025-03-14 11:30:03 +0800

    First push

---
+++ TeaAPI/Repositories/Products/Interfaces/IProductSizeRepository.cs
@@ -0,0 +1,12 @@
+﻿using TeaAPI.Models.Products;
+
+namespace TeaAPI.Repositories.Products.Interfaces
+{
+    public interface IProductSizeRepository
+    {
+        Task<IEnumerable<ProductSizePO>> GetAllAsync();
+        Task<IEnumerable<ProductSizePO>> GetByProductIdAsync(int productId, bool includeDeleted = false);
+        Task UpdateSizesAsync(int productId, IEnumerable<ProductSizePO> sizes);
+        Task DeleteByProductIdAsync(int productId);
+    }
+}
